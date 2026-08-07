b84986ab38a5369ed67df81f48b22fc4e9be5191
Dan Kirvy Manongsong <dankirvymanongsong@gmail.com>
2026-08-07 14:55:22 +0800

    Switch media storage to Cloudinary for Vercel compatibility

--- Mushroom Capstone/Capstone-Project/mushroom_dashboard/mushroom_dashboard/settings.py
+++ Mushroom Capstone/Capstone-Project/mushroom_dashboard/mushroom_dashboard/settings.py
@@ -67,7 +67,18 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+    'cloudinary_storage',
+    'django.contrib.staticfiles',
+    'cloudinary',
 ]
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Site ID for allauth
 SITE_ID = 1
