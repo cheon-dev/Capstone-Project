b61357923fd8e6bb5856b944ac58d62ca846c0f5
Dan Kirvy Manongsong <dankirvymanongsong@gmail.com>
2026-08-07 14:17:21 +0800

    Disable persistent DB connections for serverless compatibility

--- Mushroom Capstone/Capstone-Project/mushroom_dashboard/mushroom_dashboard/settings.py
+++ Mushroom Capstone/Capstone-Project/mushroom_dashboard/mushroom_dashboard/settings.py
@@ -148,7 +148,7 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 
 if DATABASE_URL:
     DATABASES = {
-        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600),
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=0),
     }
     DATABASES['default']['OPTIONS'] = {
         'ssl': {'ca': str(BASE_DIR / 'ca.pem')}
